d111468a9a5574fc69bbe6540b811ce7db072f5f
Renée Kooi <renee@kooi.me>
2018-03-01 22:57:36 +0100

    export default middleware

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { REQUEST_START, REQUEST_COMPLETE } from './actionTypes'
 import middleware from './middleware'
 import { request, get, post, put, del } from './actions'
 
+export default middleware
 export {
   middleware,
   request,
